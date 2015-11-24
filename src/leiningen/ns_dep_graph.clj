f6005221d3e5347ac8f5ddf8be05a2d6c61034bb
Daniel Compton <desk@danielcompton.net>
2015-11-24 20:58:43 +1300

    Allow passing a platform argument to generate graph for CLJS
    This required an upgrade to tools.namespace too. The new version of
    tools.namespace requires tools.reader 0.10.0, but when I was testing
    this there was another version of tools.reader coming from somewhere
    which was conflicting. I've opened a ticket against Leiningen to upgrade
    their clj-http dependency as I suspect it could be this one.
    
    I've vendored read-file-ns-decl so that it prints out exceptions that
    get thrown. I'm not 100% sure if this is the right option, but I don't
    like how it swallows all exceptions.
    
    Fixes #1

--- src/leiningen/ns_dep_graph.clj
+++ src/leiningen/ns_dep_graph.clj
@@ -4,19 +4,37 @@
             [clojure.tools.namespace.file :as ns-file]
             [clojure.tools.namespace.track :as ns-track]
             [clojure.tools.namespace.find :as ns-find]
+            [clojure.tools.namespace.parse :as parse]
             [clojure.tools.namespace.dependency :as ns-dep]
-            [rhizome.viz :as viz]))
+            [rhizome.viz :as viz])
+  (:import (java.io PushbackReader)))
+
+(defn read-file-ns-decl
+  "Attempts to read a (ns ...) declaration from file, and returns the
+  unevaluated form. Returns nil if read fails due to invalid syntax or
+  if a ns declaration cannot be found. read-opts is passed through to
+  tools.reader/read."
+  ([file]
+   (read-file-ns-decl file nil))
+  ([file read-opts]
+   (with-open [rdr (PushbackReader. (io/reader file))]
+     (try (parse/read-ns-decl rdr read-opts)
+          (catch Exception e (println "Exception while parsing ns decl:" e))))))
 
 (defn ns-dep-graph
   "Create a namespace dependency graph and save it as ns-dep-graph.png."
-  [project & args]
-  (let [source-files (apply set/union
-                            (map (comp ns-find/find-clojure-sources-in-dir
+  [project & [platform-kw]]
+  (let [platform (case (read-string platform-kw)
+                   :clj ns-find/clj
+                   :cljs ns-find/cljs
+                   ns-find/clj)
+        source-files (apply set/union
+                            (map (comp #(ns-find/find-sources-in-dir % platform)
                                        io/file)
                                  (project :source-paths)))
         tracker (ns-file/add-files {} source-files)
         dep-graph (tracker ::ns-track/deps)
-        ns-names (set (map (comp second ns-file/read-file-ns-decl)
+        ns-names (set (map (comp second read-file-ns-decl)
                            source-files))
         part-of-project? (partial contains? ns-names)
         nodes (filter part-of-project? (ns-dep/nodes dep-graph))]
